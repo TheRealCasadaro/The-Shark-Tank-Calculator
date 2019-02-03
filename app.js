faeaeedb3c9818fb9182edc74041c1e2cb074ae2
TheRealCasadaro <TheRealCasadaro@TheRealCasadaro.com>
2019-02-02 21:53:52 -0800

    basic fucntionality complete

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 
-class Valuation {
-  constructor(percent, askingAmount) {
-  this.percent = percent
-  this.amount = askingAmount
-  this.result = function () {
-    return this.formatter((100 / this.percent) * this.amount)
-  }
-}
+const calcButton = document.getElementById('calculateButton')
 
+calcButton.addEventListener('click', getValuation)
 
-formatter (int) {
+function getValuation(){
+  const calcContainer = document.getElementById('calcContainer')
+  const percentage = document.getElementById('percentage').value
+  const askingAmount = document.getElementById('askingAmount').value
+  let valuation = ((100 / Number(percentage)) * Number(askingAmount))
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
   })
-  let value = formatter.format(int)
-  return value
+
+
+  calcContainer.innerHTML = `<h1>${formatter.format(valuation)}</h1>`
 }
-
-}
-
-
-let businessOne = new Valuation(25, 250000)
-
-console.log(businessOne.result())
